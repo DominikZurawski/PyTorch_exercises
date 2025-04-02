46d61b710fd1d43c3a2c425a53d6417ba379465c
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2025-04-02 11:59:50 +0200

    ViT Transformer

--- going_modular/engine.py
+++ going_modular/engine.py
@@ -2,9 +2,9 @@
 Contains functions for training and testing a PyTorch model.
 """
 import torch
-
+from torch.utils.tensorboard import SummaryWriter
 from tqdm.auto import tqdm
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple,  Optional
 
 def train_step(model: torch.nn.Module, 
                dataloader: torch.utils.data.DataLoader, 
@@ -123,7 +123,7 @@ def train(model: torch.nn.Module,
           loss_fn: torch.nn.Module,
           epochs: int,
           device: torch.device, 
-          writer: torch.utils.tensorboard.writer.SummaryWriter # new parameter to take in a writer
+          writer: Optional[torch.utils.tensorboard.writer.SummaryWriter] = None
           ) -> Dict[str, List]:
     """Trains and tests a PyTorch model.
 
